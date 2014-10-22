62f8d68f6b8da8c3b141d978acc82c948deac6c6
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-22 17:21:56 +0300

    Added regexes for issue and project discovery based on link, to be continued with user filtering based on issue/project

--- assets/javascripts/redmine-mention.js
+++ assets/javascripts/redmine-mention.js
@@ -1,11 +1,21 @@
 $(function () {
-
+  var issue_regex_match = location.href.match(/\/issues\/(.*)\?/);
+  var issue_id = issue_regex_match ? issue_regex_match[1] : false;
+  var project_regex_match = location.href.match(/\/projects\/(.*)\/issues/) || location.href.match(/\/projects\/(.*)\/wiki/);
+  var project_identifier = project_regex_match ? project_regex_match[1] : false;
   $('.controller-issues textarea.wiki-edit, .controller-wiki textarea.wiki-edit').mentionsInput({
     onDataRequest:function (mode, query, callback) {
       var data = [];
+      var request_data = {'search_tag': query};
+      if (issue_id) {
+        request_data['issue_id'] = issue_id;
+      }
+      if (project_identifier) {
+        request_data['project_identifier'] = project_identifier;
+      }
       $.ajax({
         url: '/mention/search',
-        data: {'search_tag': query},
+        data: request_data,
         success: function(result) {
           callback.call(this, result['users']);
         }
