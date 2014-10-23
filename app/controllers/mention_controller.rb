d980880c0b339c74761629157157002aba4ad8d4
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-10-23 15:57:59 +0300

    Implemented mentions for members only

--- app/controllers/mention_controller.rb
+++ app/controllers/mention_controller.rb
@@ -1,9 +1,17 @@
 class MentionController < ApplicationController
   def search
-    result = User.with_name(params[:search_tag]).each.inject([]) do |data, user|
-                data << {id: user.id, username: user.login, full_name: user.name, email: user.mail} unless user.locked?
-                data
-             end
+    issue_id = params[:issue_id]
+    project_id = params[:project_id]
+    users = []
+    if project_id
+      users = User.with_name(params[:search_tag]).includes(:members).where('members.project_id = ?', project_id)
+    elsif issue_id
+      users = User.with_name(params[:search_tag]).includes(members: {project: :issues}).where('issues.id = ?', issue_id)
+    end
+    result = users.each.inject([]) do |data, user|
+      data << {id: user.id, username: user.login, full_name: user.name, email: user.mail} unless user.locked?
+      data
+    end
     render json: {users: result}
   end
 end
