8bde47da83173223a127444690448dc10ca1b991
Roman Belyakovsky <ihryamzik@gmail.com>
2016-08-05 03:36:13 +0300

    Changed mention pattern from [~username] to @username

--- lib/mention.rb
+++ lib/mention.rb
@@ -12,18 +12,18 @@ require 'mention/issue_patch'
 require 'mention/application_helper_patch'
 
 module Mention
-  TAG_SCAN_REGEX = /\[\~\w+\]/
+  TAG_SCAN_REGEX = /\@([\w+\.]+\w)/
 
   def self.update_tag(content, watchable=nil, only_path=true)
     mentioned_users = content.scan(TAG_SCAN_REGEX)
     mentioned_users.each do |mentioned_user|
-      if user = User.find_by_login(mentioned_user[2..-2])
+      if user = User.find_by_login(mentioned_user[0])
         if watchable.is_a?(Issue) or watchable.is_a?(WikiPage)
           Watcher.create(:watchable => watchable, :user => user)
         else
           url_for_options = {:controller => 'users', :action => 'show', :id => user.id, :only_path => only_path}
           url_for_options = url_for_options.merge(Mailer.default_url_options) unless only_path
-          content = content.gsub(mentioned_user, "<a class='user active' href='#{Rails.application.routes.url_helpers.url_for(url_for_options)}'>#{user.name}</a>")
+          content = content.gsub("@" + mentioned_user[0], "<a class='user active' href='#{Rails.application.routes.url_helpers.url_for(url_for_options)}'>#{user.name}</a>")
         end
       end
     end
@@ -32,7 +32,7 @@ module Mention
 
   def self.mentioned_users(content)
     content.scan(TAG_SCAN_REGEX).map do |mentioned_user|
-      User.find_by_login(mentioned_user[2..-2])
+      User.find_by_login(mentioned_user[0])
     end
   end
 
