2e78a9ba2de73effea724a441956e9d749a0933a
Mayank Jain <mjain@isystango.com>
2017-01-04 12:55:15 +0530

    Added compatibility with Rails 4

--- app/controllers/mention_controller.rb
+++ app/controllers/mention_controller.rb
@@ -1,9 +1,17 @@
 class MentionController < ApplicationController
   def search
-    result = User.with_name(params[:search_tag]).each.inject([]) do |data, user|
-                data << {id: user.id, username: user.login, full_name: user.name, email: user.mail} unless user.locked?
-                data
-             end
+  	result = []
+
+  	if params[:search_tag].present?
+	  	firstname = params[:search_tag].to_s.split(/\s/)[0]
+	  	lastname = params[:search_tag].to_s.split(/\s/)[1]
+
+	    result = User.where("firstname like ? AND lastname like ?", "%#{firstname}%", "%#{lastname}%").each.inject([]) do |data, user|
+	      data << {id: user.id, username: user.login, full_name: user.name, email: user.mail} unless user.locked?
+	      data
+	    end
+	  end
+
     render json: {users: result}
   end
 end
